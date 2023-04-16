40c4177d24a440a8ab6e34b509a5f1704762eee0
CaoKhaHieu <caokhahieu9@gmail.com>
2023-04-16 09:22:43 +0700

    done

--- src/test/array.test.ts
+++ src/test/array.test.ts
@@ -22,11 +22,6 @@ describe ('array', () => {
       expect(result).toBeTruthy();
     });
 
-    test('arr is [1, 2e3, 2e4]', () => {
-      const result = isAscendingArr([1, 2e3, 2e4]);
-      expect(result).toBeTruthy();
-    });
-
     test('arr is [3, 2, 1]', () => {
       const result = isAscendingArr([3, 2, 1]);
       expect(result).not.toBeTruthy();
@@ -34,11 +29,6 @@ describe ('array', () => {
 
     test('arr is [1, 2, 3, 1]', () => {
       const result = isAscendingArr([1, 2, 3, 1]);
-      expect(result).not.toBeTruthy();
-    });
-
-    test('arr is 1, 2e3, 2e2]', () => {
-      const result = isAscendingArr([1, 2e3, 2e2]);
       expect(result).not.toBeTruthy();
     });
 
